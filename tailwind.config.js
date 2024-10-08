7cfadb414979a4d3adaf484c5bc5c7e9709b366d
Anis Hadj Ramdane <anis28hr@gmail.com>
2024-10-08 19:20:12 +0100

    Add files via upload
    React + vite 
    Convert from BTC to another currency

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  darkMode: 'class', // Enable dark mode using a class
+
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
